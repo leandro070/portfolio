8f3195f6cd0e4e40539b170c5fa7ee985d15276d
Leandro Gutierrez <leandro.gutierrez@thelucky.app>
2024-11-05 23:26:53 -0300

    Porfolio v2

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				display: ["Bebas Neue"],
+				body: ["Roboto"]
+			},
+			colors: {
+				primary: "var(--color-white)",
+				secondary: "var(--color-black)",
+				accent: "var(--color-purple)",
+				grey: "var(--color-grey)",
+				['light-blue']: "var(--color-light-blue)"
+			}
+		},
+	},
+	plugins: [],
+}
