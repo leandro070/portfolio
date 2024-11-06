7ea6faa96842f5d9d6856ba41cbc0a5acd66ddec
Leandro Gutierrez <leandro.gutierrez@thelucky.app>
2024-11-05 23:32:25 -0300

    configure astro and github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://leandro070.github.io/',
+  base: 'portfolio'
 });
